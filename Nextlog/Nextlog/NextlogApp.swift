6f8710b8ded18ce0581fef9f82fd5b233fe352aa
Telha Wasim <telhawasim@gmail.com>
2025-03-19 21:45:24 +0500

    first commit

---
+++ Nextlog/Nextlog/NextlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextlogApp.swift
+//  Nextlog
+//
+//  Created by Telha Wasim on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct NextlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
